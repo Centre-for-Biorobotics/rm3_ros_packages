29f7e677118ac8fbb2b100254f5910d4a5e2f863
RozaGkliva <roza.gkliva@ttu.ee>
2020-08-12 18:27:08 +0300

    Added launch file for single motor interfacing.

---
+++ launch/single_motor.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+	return LaunchDescription([
+		Node(
+			package='test_python_package', 
+			namespace='motor0',
+			executable='serial_interface',
+			name='serial_interface',
+			output='screen',
+			parameters=[{'port': '/dev/ttyACM0'}]
+			)
+		])
