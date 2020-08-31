731dc087ab8785496cdf42c5ad12b7ecf6ca9837
RozaGkliva <roza.gkliva@ttu.ee>
2020-08-31 16:51:59 +0300

    Started open loop steering for remotely operated vehicle.

---
+++ steering/steering/open_loop_steering.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" 
+
+@author: Roza Gkliva
+@contact: roza.gkliva@ttu.ee
+@creation date: 29-08-2020 (started)
+
+"""
+
+import rclpy
+
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+
+
+
+class OpenLoopSteering(Node):
+	def __init__(self):
+		super().__init__('open_loop_steering')
+		self.sub_joystick = self.create_subscription(Twist, 'cmd_vel', self.joystickCallback, 10)
+
+
+	def joystickCallback(self, msg):
+		self.get_logger().info('twist data: "%d"' %msg.linear.x )
+
+	def speedsBroadcast(self):
+		return
+
+
+
+
+
+
+
+def main(args=None):
+	rclpy.init(args=args)
+	open_loop_steering = OpenLoopSteering()
+
+	rclpy.spin(open_loop_steering)
+
+
+	open_loop_steering.destroy_node()
+	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
