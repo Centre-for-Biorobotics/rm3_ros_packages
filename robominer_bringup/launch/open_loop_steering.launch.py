df5903d94eb970bb1a08d883b316407cb7f9b37e
RozaGkliva <roza.gkliva@ttu.ee>
2020-09-29 16:19:53 +0300

    Refactored launching process to use 'robominer_bringup' for high level launching management

---
+++ robominer_bringup/launch/open_loop_steering.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+import os
+
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+
+
+def generate_launch_description():
+	serial_peripherals = launch.actions.IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			os.path.join(get_package_share_directory('robominers_serial_interface'),
+			'launch',
+			'comms_test.launch.py')
+		))
+	steering_stuff = launch.actions.IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			os.path.join(get_package_share_directory('steering'),
+			'launch',
+			'open_loop_steering.launch.py')
+		))
+	return launch.LaunchDescription([
+		serial_peripherals,
+		steering_stuff
+		])
--- steering/launch/open_loop_steering.launch.py
+++ steering/launch/open_loop_steering.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 import os
-# from launch_ros.actions import Node
-# from launch_ros.actions import IncludeLaunchDescription
+
 import launch
 import launch.actions
 import launch.substitutions
@@ -13,12 +12,6 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-	included_serial_launch = launch.actions.IncludeLaunchDescription(
-		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('robominers_serial_interface'),
-			'launch',
-			'comms_test.launch.py')
-		))
 	steering_node = launch_ros.actions.Node(
 		package='steering', 
 		executable='open_loop_steering',
@@ -33,7 +26,6 @@ def generate_launch_description():
 		output='screen'
 		)
 	return launch.LaunchDescription([
-		included_serial_launch,
 		steering_node,
 		turtlesim_node
 		])
