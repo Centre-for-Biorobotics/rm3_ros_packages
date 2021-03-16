b0be4f86f994ad02be0815cb7d7592353334304d
RozaGkliva <roza.gkliva@ttu.ee>
2021-03-16 16:20:18 +0200

    Added motor rpm setpoint publishers in steering node.
    Also some refactoring.

--- steering/steering/open_loop_steering.py
+++ steering/steering/open_loop_steering.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ 
+Takes robot body velocities as input from joystick. 
+Calculates inverse kinematics to determine screw velocities.
+Publishes screw velocities
 
 @author: Roza Gkliva
 @contact: roza.gkliva@ttu.ee
@@ -19,10 +22,10 @@ from robominer_msgs.msg import MotorModuleCommand
 
 import numpy as np
 
-# motors = np.array(['front_right',
-# 			'rear_right',
-# 			'rear_left',
-# 			'front_left'])
+motors = np.array(['front_right',
+			'rear_right',
+			'rear_left',
+			'front_left'])
 
 
 # rows: motor modules {fl, fr, rl, rr}
@@ -36,9 +39,14 @@ import numpy as np
 class OpenLoopSteering(Node):
 	def __init__(self):
 		super().__init__('open_loop_steering')
-		self.lx = 100 			# mm longitudinal distance
-		self.ly = 300 			# mm lateral distance
-		self.screw_radius = 55 	# mm
+		self.lx = 0.1 			# m longitudinal distance
+		self.ly = 0.3 			# m lateral distance
+		self.screw_radius = 0.055 	# m
+		self.kinematics_timer_period = 0.1 # seconds
+		self.cmd_vel_x = 0
+		self.cmd_vel_y = 0
+		self.cmd_vel_yaw = 0
+
 
 		# fr rr rl fl
 		self.platform_kinematics = np.array([
@@ -50,6 +58,9 @@ class OpenLoopSteering(Node):
 		self.sub_joystick = self.create_subscription(Joy, 'joy', self.joystickCallback, 10)
 		self.publisher_motor0_commands = self.create_publisher(MotorModuleCommand, '/motor0/motor_rpm_setpoint', 10)
 		self.publisher_motor1_commands = self.create_publisher(MotorModuleCommand, '/motor1/motor_rpm_setpoint', 10)
+		self.publisher_motor2_commands = self.create_publisher(MotorModuleCommand, '/motor2/motor_rpm_setpoint', 10)
+		self.publisher_motor3_commands = self.create_publisher(MotorModuleCommand, '/motor3/motor_rpm_setpoint', 10)
+		self.kinematics_timer = self.create_timer(self.kinematics_timer_period, self.inverseKinematics)
 
 
 	def joystickCallback(self, msg):
@@ -61,27 +72,33 @@ class OpenLoopSteering(Node):
 		self.cmd_vel_x = msg.axes[1]
 		self.cmd_vel_y = msg.axes[0]
 		self.cmd_vel_yaw = msg.axes[2]
+
+
+	def inverseKinematics(self):
 		self.robot_twist = [self.cmd_vel_x, self.cmd_vel_y, self.cmd_vel_yaw]
 
-		self.screw_speeds = 10 * 1/self.screw_radius * np.dot(self.platform_kinematics, self.robot_twist)
+		self.screw_speeds = 1/self.screw_radius * np.dot(self.platform_kinematics, self.robot_twist)
 		
 		# self.get_logger().info('x: "%f", y: "%f", yaw: "%f"' %( self.cmd_vel_x, self.cmd_vel_y, self.cmd_vel_yaw))
-
-		# self.get_logger().info('fr: "%f", rr: "%f", rl: "%f", fl: "%f"' %( self.screw_speeds[0], self.screw_speeds[1], self.screw_speeds[2], self.screw_speeds[3]))
-		for motor in range(4):
-			self.motor_cmd = MotorModuleCommand()
-
-			self.motor_cmd.motor_rpm_goal = self.screw_speeds[motor]
-			# self.motor_cmds.rr_motor_rpm_goal = self.screw_speeds[1]
-			# self.motor_cmds.rl_motor_rpm_goal = self.screw_speeds[2]
-			# self.motor_cmds.fl_motor_rpm_goal = self.screw_speeds[3]
-			self.publisher_motor0_commands.publish(self.motor_cmd)
-
-	def inverseKinematics(self, x, y, yaw):
-		return
+		self.get_logger().info('fr: "%f", rr: "%f", rl: "%f", fl: "%f"' %( self.screw_speeds[0], self.screw_speeds[1], self.screw_speeds[2], self.screw_speeds[3]))
+		
+		self.speedsBroadcast()
 
 
 	def speedsBroadcast(self):
+		'''
+		Publishes the results of inv.kin to 4 topics, one for each screw
+		'''
+		self.motor_cmd = [MotorModuleCommand() for i in range (4)]
+		# self.get_logger().info(str(self.motor_cmd))
+		for m in range(4):
+			self.motor_cmd[m].motor_id = motors[m]
+			self.motor_cmd[m].motor_rpm_goal = self.screw_speeds[m]
+		# self.get_logger().info(str(self.motor_cmd))
+		self.publisher_motor0_commands.publish(self.motor_cmd[0])
+		self.publisher_motor1_commands.publish(self.motor_cmd[1])
+		self.publisher_motor2_commands.publish(self.motor_cmd[2])
+		self.publisher_motor3_commands.publish(self.motor_cmd[3])
 		return
 
 
