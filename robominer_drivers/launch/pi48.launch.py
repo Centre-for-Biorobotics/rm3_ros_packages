2b3405c51c37beaafd3ca8520db0723fce0e3791
RozaGkliva <roza.gkliva@ttu.ee>
2022-03-23 19:38:04 +0200

    Added complementary filter from 'imu_tools' to launch file to compute orientation.
    NB! imu_tools foxy version hasn't been released yet, but the eloquent version compiles and runs fine.
    - also added static transform for visualization of the imu reference frame.

--- robominer_drivers/launch/pi48.launch.py
+++ robominer_drivers/launch/pi48.launch.py
@@ -11,9 +11,28 @@ def generate_launch_description():
         parameters=[
             {'device_name': 'pi48'},
             {'device_hwid': '0403:6010'}
-        ]
+        ],
+        remappings=[("/imu/data_raw", "/pi48_imu/data_raw")]
+    )
+    complementary_filter = Node(
+        # needs imu_tools currently in https://github.com/ccny-ros-pkg/imu_tools
+        package='imu_complementary_filter',
+        executable='complementary_filter_node',
+        name='complementary_filter',
+        output='screen',
+        remappings=[
+            ("/imu/data_raw", "/pi48_imu/data_raw"),
+            ("/imu/data", "/pi48_imu/data")]
+    )
+    static_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments = ["0", "0", "0", "0", "0", "0", "map", "pi48"]
     )
 
     return LaunchDescription(
-        [pi48_imu]
+        [pi48_imu,
+        complementary_filter,
+        static_transform
+        ]
     )
