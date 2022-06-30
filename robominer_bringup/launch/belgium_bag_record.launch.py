7edc5bae48209d45aef6e8b4b6a5d643df6e8446
RozaGkliva <roza.gkliva@ttu.ee>
2022-06-30 15:24:31 +0300

    Updated bag recording launch file.

--- robominer_bringup/launch/belgium_bag_record.launch.py
+++ robominer_bringup/launch/belgium_bag_record.launch.py
@@ -5,19 +5,10 @@ def generate_launch_description():
     bag_recording = launch.actions.ExecuteProcess(
         cmd=[
             'ros2', 'bag', 'record',
-            '/aruco_detect/field_transforms',
-            '/aruco_detect/robot_transforms',
-            '/camera/color/image_raw',
-            '/camera/imu',
+            '/aruco_detect/robot_tag_transforms',
             '/tf',
             '/tf_static',
-            # '/robot_euler',
-            # '/robot_pose',
             '/joy',
-            # '/front_left/parameter_events',
-            # '/rear_left/parameter_events',
-            # '/front_right/parameter_events',
-            # '/rear_right/parameter_events',
             '/motor0/motor_rpm_setpoint',
             '/motor1/motor_rpm_setpoint',
             '/motor2/motor_rpm_setpoint',
@@ -29,7 +20,6 @@ def generate_launch_description():
             '/temperature_sensor/temperature',
             '/bno080_imu',
             '/pi48_imu/data_raw',
-            '/pi48_imu/complementary_data',
             '/whiskers',
             '/spectrometer_left/spectrometer_frequency_command',
             '/spectrometer_left/spectrometer_calibration',
@@ -38,7 +28,11 @@ def generate_launch_description():
             '/spectrometer_right/spectrometer_frequency_command',
             '/spectrometer_right/spectrometer_calibration',
             '/spectrometer_right/reflectance_reading',
-            '/spectrometer_right/fluorescence_reading'
+            '/spectrometer_right/fluorescence_reading',
+            '/mapData',
+            '/WhiskerPointCloud',
+            '/camera_jetson/color/image_raw',
+            '/robot_path'
             ],
         output='screen'
         )
