f251dc876e52532efcea9adc338d14cf7c68e87d
Tanel Kossas <tanel.kossas@gmail.com>
2023-03-31 14:31:55 +0300

    Remove magic nums from directional weight function

--- robominer_locomotion_control/robominer_locomotion_control/pathfinder/util/whisker_helpers.py
+++ robominer_locomotion_control/robominer_locomotion_control/pathfinder/util/whisker_helpers.py
@@ -73,27 +73,28 @@ def create_adjusted_whisker_matrix(whiskers: List[Whisker], offset_whisker_matri
     return [_list if any(_list) else None for _list in whisker_matrix]
 
 
-def whisker_multiplier(col_num: int, reverse: bool=False):
+def directional_whisker_weight(col_num: int, whiskers_in_row: int):
     """
+    Assumed that whiskers are spaced roughly equally and there's an even amount.
     Convert 4-7 to 1..4 and 0-3 to -4..-1 for finding appropriate turning radius
     Returns: Value from -4..4 excluding 0
     """
-    if reverse:
-        col_num = 7 - col_num
+    if whiskers_in_row % 2 == 1:
+        return col_num - whiskers_in_row // 2
 
-    if col_num < 4:
-        return (col_num - 4)
+    if col_num >= whiskers_in_row // 2:
+        return col_num - whiskers_in_row // 2
     
-    return (col_num - 3) ** 1
+    return col_num - whiskers_in_row // 2 - 1
 
 
-def calc_whiskers_inclination_euclid(whiskers : List[Whisker], reverse: bool=False):
+def calc_whiskers_inclination_euclid(whiskers : List[Whisker]):
     """
     Calculate an inclination for a whisker array, taking into account the position of the whiskers.
     Positive if higher columns have higher values, negative if lower columns have higher values.
     Values are between [-10; 10]
     """
-    return sum([whisker_euclid_dist(w) * whisker_multiplier(w.pos.col_num, reverse) for w in whiskers])
+    return sum([whisker_euclid_dist(w) * directional_whisker_weight(w.pos.col_num, len(whiskers)) for w in whiskers]) / (2 * sum(range(1, len(whiskers) // 2)))
 
 
 def calc_whisker_pressure_max(whiskers: List[Whisker]):
@@ -115,8 +116,15 @@ def whisker_euclid_dist(whisker: Whisker):
     Calculate the euclidean distance of the whisker's x and y displacement.
     Normalized to [0, 1]
     """
+
     if POLAR_INPUT:
-        return np.clip(abs(whisker.y) * 0.6 / 40, 0, 0.6) + np.clip(abs(whisker.z) * 0.4 / 25., 0., 0.4)
+        w_theta = 0.6
+        w_z = 0.4
+
+        max_theta = 40
+        max_z = 25
+
+        return np.clip(abs(whisker.y) / max_theta, 0, 1) * w_theta + np.clip(abs(whisker.z) / max_z, 0., 1) * w_z
     
     magnitude, _ = polar(whisker.x, whisker.y)
     return np.clip(abs(magnitude) * 1.3, 0., 1.)
