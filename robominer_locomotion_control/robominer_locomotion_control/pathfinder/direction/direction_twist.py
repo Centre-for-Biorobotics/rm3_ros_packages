fc6b7f30c79bf08a0153175721ccd3aaf74a9b56
Tanel Kossas <tanel.kossas@gmail.com>
2023-03-31 14:32:43 +0300

    Change pathfinder values according to experiments

--- robominer_locomotion_control/robominer_locomotion_control/pathfinder/direction/direction_twist.py
+++ robominer_locomotion_control/robominer_locomotion_control/pathfinder/direction/direction_twist.py
@@ -2,9 +2,9 @@
 from enum import Enum
 
 class DirectionTwist(Enum):
-    MOVE_LEFT       = [ 0,  1,  0]
-    MOVE_RIGHT      = [ 0, -1,  0]
-    MOVE_FORWARD    = [ 1,  0,  0]
-    MOVE_BACKWARD   = [-1,  0,  0]
-    TURN_LEFT       = [ 0,  0, -1]
-    TURN_RIGHT      = [ 0,  0,  1]
+    MOVE_LEFT       = [  0,  50,  0]
+    MOVE_RIGHT      = [  0, -50,  0]
+    MOVE_FORWARD    = [ 50,   0,  0]
+    MOVE_BACKWARD   = [-50,   0,  0]
+    TURN_LEFT       = [  0,   0, -1]
+    TURN_RIGHT      = [  0,   0,  1]
