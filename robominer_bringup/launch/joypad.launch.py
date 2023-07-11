a35a7104af64e91a8d49cdf40cf7ab80608ae048
RozaGkliva <roza.gkliva@ttu.ee>
2023-07-11 18:51:02 +0300

    Remapping joystick topic so it doesn't interfere with forward kinematics and dynamics.

--- robominer_bringup/launch/joypad.launch.py
+++ robominer_bringup/launch/joypad.launch.py
@@ -11,19 +11,20 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # teleop_node = launch_ros.actions.Node(
-    # 	package='teleop_twist_joy',
-    # 	executable='teleop_node',
-    # 	name='teleop',
-    # 	output='screen'
-    # 	)
-    teleop_joy = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('teleop_twist_joy'),
-                'launch',
-                'teleop-launch.py')
-        ))
+    
+    teleop_joy = launch.actions.GroupAction(
+        actions=[
+            launch_ros.actions.SetRemap(src='/cmd_vel',dst='/cmd_vel_joy'),
+
+        launch.actions.IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('teleop_twist_joy'),
+                    'launch',
+                    'teleop-launch.py')))
+        ]
+    )
+
     return launch.LaunchDescription([
         teleop_joy
         ])
